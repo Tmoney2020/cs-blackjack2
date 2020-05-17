7de95aed04c72a8d7747fb88226dd756fd1c4a55
Trent Costa <trentcosta@gmail.com>
2020-05-17 11:19:13 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cs_blackjack2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to C#");
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# cs_blackjack2
